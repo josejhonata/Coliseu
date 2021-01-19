c3fe435803ae7409691963687aef267c2f6f8bfa
josejhonata <josetrevisan04@gmail.com>
2021-01-19 20:09:12 -0300

    GH-08 Criando os models e factory dos equipamentos

---
+++ app/Http/Controllers/EquipamentoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\equipamento;
+use Illuminate\Http\Request;
+
+class EquipamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\equipamento  $equipamento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(equipamento $equipamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\equipamento  $equipamento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(equipamento $equipamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\equipamento  $equipamento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, equipamento $equipamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\equipamento  $equipamento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(equipamento $equipamento)
+    {
+        //
+    }
+}
---
+++ database/factories/EquipamentoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\equipamento;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EquipamentoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = equipamento::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nome' => $this->faker->name,
+        ];
+    }
+}
