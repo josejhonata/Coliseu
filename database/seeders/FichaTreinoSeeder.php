b6b1d68f66091cf3e40fc2106fde1e11b06f81ef
Patrick-Santana <zonaabissal2016@gmail.com>
2021-01-22 14:12:46 -0300

    GH-09 Seeder de Ficha de Treinamento

--- database/seeders/FichaTreinoSeeder.php
+++ database/seeders/FichaTreinoSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ficha_treino;
+use App\Models\equipamento;
 use Illuminate\Database\Seeder;
 
 class FichaTreinoSeeder extends Seeder
@@ -13,6 +15,15 @@ class FichaTreinoSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Ficha_treino::factory(10)->create();
+
+        $ficha_treino = Ficha_treino::all();
+
+        foreach ($ficha_treino as $Ficha_treino) {
+            # code...
+            Ficha_treino::factory(10)->create([
+                'ficha_treinos' => $ficha_treino->id
+            ]);
+        }
+
     }
 }
