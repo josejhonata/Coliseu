f88df5f49178770a7d3864b4c8abd3ca35382609
Patrick-Santana <zonaabissal2016@gmail.com>
2021-01-26 14:16:28 -0300

    GH-11 Adicionando class logo coliseu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,10 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            <a href="/" >
+
+                <img class="h-16 " src="{{ asset('dark.png') }}" />
+
             </a>
         </x-slot>
 
