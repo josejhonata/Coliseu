bc4c2af45953ce7ae962cbee86bccf9dd09762e9
Patrick-Santana <zonaabissal2016@gmail.com>
2021-01-26 13:21:38 -0300

    GH-06 Alterando login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('Senha')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -44,7 +44,7 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Esqueceu sua senha?') }}
                     </a>
                 @endif
 
