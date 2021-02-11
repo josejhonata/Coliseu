3f4e0a9af207afce23aee197d73cf6edd1a46861
erinston <erinstong@gmail.com>
2021-02-11 10:51:50 -0300

    Corrigindo conflitos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,42 @@
 <x-app-layout>
+    <style>
+
+         .button1{
+                line-height: 22px;
+                cursor: pointer;
+                font-weight: 500;
+                display: inline-flex;
+                text-decoration: none;
+                background-color:  #d85c23;
+                color: white;
+                border-radius: 28px;
+                font-size: 20px;
+                margin-right: 10px;
+                padding: 16px 32px;
+            }
+            .button1:hover{
+                background-color: #131313;
+                box-shadow: 2px 2px 4px;
+            }
+            .button2{
+                line-height: 22px;
+                cursor: pointer;
+                font-weight: 500;
+                display: inline-flex;
+                background-color:  #d85c23;
+                text-decoration: none;
+                color: white;
+                border-radius: 28px;
+                font-size: 20px;
+                margin-left: 10px;
+                padding: 16px 32px;
+            }
+            .button2:hover{
+                background-color: #131313;
+                box-shadow: 2px 2px 4px;
+
+            }
+    </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Coliseu') }}
@@ -17,7 +55,7 @@
 
             </div>
         </div>
-       
+
 <div class="fixed z-10 inset-0 overflow-y-auto" x-show="add_modal" >
   <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
@@ -26,7 +64,7 @@
     </div>
 
     <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
- 
+
     -->
     <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
       <div class="p-3">
@@ -126,85 +164,6 @@
       <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
     </div>
 
-    <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
- 
-    -->
-    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-      <div class="p-3">
-        <h1>  Cadastrar Aluno</h1>
 
-        <form action="{{route('registro')}}" method="POST">
-            @csrf
-
-
-            <div>
-                <x-label for="tipo_id" :value="__('Tipo_Id')" />
-
-                <x-input id="tipo_id" class="block mt-1 w-full" type="text" name="tipo_id" :value="old('tipo_id')" required/>
-            </div>
-
-            <div>
-                <x-label for="name" :value="__('Nome')" />
-
-                <x-input id="name" class="block mt-1 w-full" placeholder="Ex: Liliane" type="text" name="name" :value="old('name')" required/>
-            </div>
-
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required/>
-            </div>
-
-
-
-            <div>
-                <x-label for="cpf" :value="__('Cpf')" />
-
-                <x-input id="cpf" class="block mt-1 w-full"  type="text" name="cpf" :value="old('cpf')" required/>
-            </div>
-
-             <div>
-                <x-label for="cep" :value="__('CEP')" />
-
-                <x-input id="cep" class="block mt-1 w-full"  type="text" name="cep" :value="old('cep')" required/>
-            </div>
-
-           <div class="mt-4">
-                <x-label for="username" :value="__('Nome de usuário')" />
-
-                <x-input id="username" class="block mt-1 w-full" placeholder="Ex: @user" type="text" name="username" :value="old('username')" required />
-            </div>
-
-
-            <div>
-                <x-label for="password" :value="__('Senha')" />
-
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password"/>
-            </div>
-
-             <div>
-                <x-label for="password_confirmation" :value="__('Confirma Senha')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required/>
-            </div>
-
-
-
-             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('dashboard') }}">
-                    {{ __('Cancelar') }}
-                </a>
-                <x-button class="ml-4">
-                    {{ __('Registre-se') }}
-                </x-button>
-            </div>
-
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-    
 </x-app-layout>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Coliseu</title>
 
         <style>
             body{
-                margin: 0;
+                margin: auto;
                 font-family: 'Work Sans', sans-serif;
-                background-color: #CDC9C9;
                 background-image: url("sdc.jpg");
+                border-image: 1px solid black;
                 -webkit-background-size: cover;
                 -moz-background-size: cover;
                 -o-background-size: cover;
@@ -18,11 +14,10 @@
 
             }
             .container{
-                width: 80%;
-                margin: 0 auto;
+                width: 100%;
             }
             header{
-                background: #4682B4;
+                background: #d85c23;
             }
             header::after{
                 content: '';
@@ -31,40 +26,49 @@
             }
             .logo{
                 float: left;
-                width: 6.4%;
-                height: 10%;
+                width: 5.7%;
+                height: 19%;
             }
             nav{
                 float: right;
             }
             nav ul{
-                margin: 0;
-                padding: 0;
+                float: right;
+                margin: 0px;
+                padding: -0;
                 list-style: none;
             }
             nav li{
+
                 display: inline-block;
-                margin-left: 70px;
+                margin-left: 7px;
                 padding-top: 25px;
                 position: relative;
             }
             nav a{
+                margin: auto;
                 color: white;
                 text-decoration: none;
-                font-size: 100%;
+                font-size: 10%;
 
             }
             nav a:hover{
-                text-shadow: 1.75px 2px 3px #87CEEB;
+                text-shadow: 1.75px 2px 3px ;
             }
             h1{
                 text-align: center;
-
                 justify-content: center;
-                font-weight: 700;
-                line-height: 325px;
+                font-weight: 70px;
+                line-height: 100px;
                 font-size: 45px;
                 color:White;
+                margin-top: 10PX;
+                text-shadow: 2px 2px 5px black;
+            }
+            .tx1{
+
+                margin-bottom:11%;
+                margin-top:10px;
             }
             p{
                 text-align: center;
@@ -76,7 +80,7 @@
                 font-weight: 500;
                 display: inline-flex;
                 text-decoration: none;
-                background-color:  #4682B4 ;
+                background-color:  #d85c23;
                 color: white;
                 border-radius: 28px;
                 font-size: 20px;
@@ -92,7 +96,7 @@
                 cursor: pointer;
                 font-weight: 500;
                 display: inline-flex;
-                background-color:  #4682B4;
+                background-color:  #d85c23;
                 text-decoration: none;
                 color: white;
                 border-radius: 28px;
@@ -103,6 +107,39 @@
             .button2:hover{
                 background-color: #131313;
                 box-shadow: 2px 2px 4px #888888;
+
+            }
+            .button {
+
+                        border: none;
+                        font-size: 20px;
+                        cursor: pointer;
+                        padding: 10px 22px;
+                        text-align: center;
+                        text-decoration: none;
+                        outline: none;
+                        display: inline-block;
+                        color: white;
+                        background-color:black;
+                        border: none;
+                        border-radius: 14px;
+
+
+            }
+
+            .button:hover {
+                background-color: white;
+                color:#d85c23;
+                text-shadow: 2px 2px 5px #d85c23;
+            }
+
+            .button:active {transform: translateY(9px);}
+
+            img{
+                width: 80px;
+            }
+            .tx1{
+                padding: 7%;
             }
         </style>
 
@@ -110,21 +147,24 @@
     <body>
         <header>
             <div class="container">
-                <a href="/"><img src="dark.png" alt="logo" class="logo"></a>
+                <a href="/" ><img class="h-14 " src="{{ asset('dark.png') }}" /></a>
                 <nav>
-                    <ul>
-                        <li><a href="{{ route('login') }}">Entrar</a></li>
-                        <li><a href="{{ route('register') }}">Cadastre-se</a></li>
+                     <ul>
+                        <li><a href="{{ route('login') }}" class="button">Entrar</a></li>
+                        <li><a href="{{ route('register') }}" class="button">Cadastre-se</a></li>
                     </ul>
                 </nav>
             </div>
         </header>
+       <div class="tx1">
+           <h1>Marque o Seu Treino em Nossa Academia</h1>
+        </div>
 
-        <h1>Marque o Seu Treino em Nossa Academia</h1>
-        <p>
+        <p style=" margin-bottom:20%;">
             <a href="{{ route('login') }}" class="button1">Marcar Treino</a>
             <a href="{{ route('login') }}" class="button2">Acompanhar Ficha de Treino</a>
         </p>
+
     </body>
 </html>
  
