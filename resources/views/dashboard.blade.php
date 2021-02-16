43f6e6a27e9399d719d32241d3e95e8529857e05
augustofarias <vinicinho1237@gmail.com>
2021-02-16 12:58:03 -0300

    GH-17 Alterando o cadastro dO aluno e de professor

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -43,6 +43,60 @@
         </h2>
     </x-slot>
 
+   @php
+$clientes=App\Models\User::where('tipo','cliente')->get();
+$clientess=App\Models\User::where('tipo','atendente')->get();
+$clientesss=App\Models\User::where('tipo','professor')->get();
+@endphp
+
+<b>
+<h1>Todos os clientes</h1>
+</b>
+<table>
+    <thead>
+        <tr>
+            <th>Id do cliente</th>
+            <th>Nome do cliente</th>
+            <th>Username do cliente</th>
+            <th>Cep do cliente</th>
+        </tr>   
+     </thead>
+    <tbody>
+        @foreach ($clientess as $cliente)
+        <tr>
+            <td>{{$cliente->id}}</td>
+            <td>{{$cliente->name}}</td>
+            <td>{{$cliente->username}}</td>
+            <td>{{$cliente->cep}}</td>
+        </tr>
+
+    @endforeach
+
+    @foreach ($clientes as $cliente)
+        <tr>
+            <td>{{$cliente->id}}</td>
+            <td>{{$cliente->name}}</td>
+            <td>{{$cliente->username}}</td>
+            <td>{{$cliente->cep}}</td>
+        </tr>
+
+    @endforeach
+
+    @foreach ($clientesss as $cliente)
+        <tr>
+            <td>{{$cliente->id}}</td>
+            <td>{{$cliente->name}}</td>
+            <td>{{$cliente->username}}</td>
+            <td>{{$cliente->cep}}</td>
+        </tr>
+
+    @endforeach
+
+    </tbody>
+</table>
+
+
+
     <div class="py-12" x-data="{add_modal:false}">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -55,6 +109,7 @@
 
             </div>
         </div>
+
 
 <div class="fixed z-10 inset-0 overflow-y-auto" x-show="add_modal" >
   <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
@@ -74,9 +129,8 @@
             @csrf
 
             <div>
-                <x-label for="tipo_id" :value="__('Tipo_Id')" />
 
-                <x-input id="tipo_id" class="block mt-1 w-full" type="text" name="tipo_id" :value="old('tipo_id')" required/>
+                <x-input id="tipo" class="block mt-1 w-full" type="hidden" name="tipo" :value="'professor'"/>
             </div>
 
             <div>
@@ -143,27 +197,75 @@
 </div>
     </div>
 
+<b>
+<h1>Cadastro de Aluno</h1>
+</b>
+<form action="{{route('registro')}}" method="POST">
+            @csrf
 
+            <div>
 
-    <div class="py-12" x-data="{add_modal:false}">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200" >
-
-                    <div class="p-3 m-0.5" @click="add_modal = true" >
-                   Cadastrar Aluno
-                   </div>
-                </div>
-
+                <x-input id="tipo" class="block mt-1 w-full" type="hidden" name="tipo" :value="'cliente'"/>
             </div>
-        </div>
-        <div class="fixed z-10 inset-0 overflow-y-auto" x-show="add_modal" >
-  <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
-    <div class="fixed inset-0 transition-opacity" aria-hidden="true" @click="add_modal = false">
-      <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-    </div>
+            <div>
+                <x-label for="name" :value="__('Nome')" />
 
+                <x-input id="name" class="block mt-1 w-full" placeholder="Ex: Liliane" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
+
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')" required/>
+            </div>
+
+
+            <div>
+                <x-label for="cpf" :value="__('Cpf')" />
+
+                <x-input id="cpf" class="block mt-1 w-full"  type="text" name="cpf" :value="old('cpf')" required/>
+            </div>
+
+             <div class="mt-4">
+                <x-label for="cep" :value="__('CEP')" />
+
+                <x-input id="cep" class="block mt-1 w-full" placeholder="Ex: 53152152" type="text" name="cep" :value="old('cep')" required />
+            </div>
+
+            <div>
+                <x-label for="username" :value="__('Nome de usuário')" />
+
+                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required/>
+            </div>
+
+
+
+
+            <div>
+                <x-label for="password" :value="__('Senha')" />
+
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password"/>
+            </div>
+
+             <div>
+                <x-label for="password_confirmation" :value="__('Confirma Senha')" />
+
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required/>
+            </div>
+
+
+
+             <div class="flex items-center justify-end mt-4">
+               <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('dashboard') }}">
+                    {{ __('Cancelar') }}
+                </a>
+                <x-button class="ml-4">
+                    {{ __('Registre-se') }}
+                </x-button>
+            </div>
+
+        </form>
 
 </x-app-layout>
 
