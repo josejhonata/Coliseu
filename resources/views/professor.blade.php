21564fee325bdbfc1eaad4e30d5cc76f6e34b206
arthur858 <arthursylva25@gmail.com>
2021-02-22 16:51:46 -0300

    GH-20 Listando equipamentos

--- resources/views/professor.blade.php
+++ resources/views/professor.blade.php
@@ -1,19 +1,3 @@
-<style type="text/css">
-table{
-width: 70%;
-padding: 5%;
-text-align: center;
-background: white;
-}
-    tr{
-
-border-bottom: 1px solid #ddd;
-
-}
-th{
-border-right: 1px solid #ddd;
-}
-</style>
 <x-app-layout>
 
 	<x-slot name="header">
@@ -25,6 +9,7 @@ border-right: 1px solid #ddd;
 @php
 $clientes=App\Models\User::where('tipo','cliente')->get();
 $fichas=App\Models\Ficha_treino::all();
+$equipamentos=App\Models\equipamento::all();
 @endphp
 <center>
 
@@ -51,27 +36,59 @@ $fichas=App\Models\Ficha_treino::all();
                 @endforeach
             </tbody>
         </table>
+
         <b>
             <h1>Fichas de treino</h1>
         </b>
         <table>
             <thead>
                 <tr style=" background: #131313; color :white;">
+                    <th>Equipamento</th>
+                    <th>Quantidade de Série</th>
+                    <th>Quantidade de repetição</th>
                     <th>Descrição</th>
-                    <th>Id do cliente</th>
+                    <th>Nome do cliente</th>
                     <th>Opções</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($fichas as $ficha)
                 <tr>
+                    <td>{{$ficha->equipamento}}</td>
+                    <td>{{$ficha->serie}}</td>
+                    <td>{{$ficha->repeticao}}</td>
                     <td>{{$ficha->descricao}}</td>
-                    <td>{{$ficha->user_id}}</td>
+                    <td>{{$ficha->user_name}}</td>
                     <td><a class="bg-red-200 rounded hover:bg-red-300" href="{{ route('rm-ficha', $ficha)}}">Excluir</a></td>
                     @endforeach
                 </tbody>
             </table>
         </center>
+
+        <center>
+    <b>
+        <h1>Todos os Equipamentos</h1>
+    </b>
+
+    <thead>
+    <table>
+            <tr style="background: #131313; color :white;">
+                <th>Nome do equipameto</th>
+                <th>Descrição do equipameto</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($equipamentos as $equipamento)
+            <tr>
+                <td>{{$equipamento->name}}</td>
+                <td>{{$equipamento->descricao}}</td>
+            </tr>
+
+            @endforeach
+
+        </tbody>
+    </table>
+</center>
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
@@ -81,6 +98,26 @@ $fichas=App\Models\Ficha_treino::all();
 
 
                                 <div>
+                                    <x-label for="equipamento" :value="__('Equipamentos do treino')" />
+
+                                    <x-input id="equipamento" class="block mt-1 w-full" type="text" name="equipamento" :value="old('equipamento')" required/>
+                                </div>
+
+
+                                <div>
+                                    <x-label for="serie" :value="__('Quantidade de serie')" />
+
+                                    <x-input id="serie" class="block mt-1 w-full" type="text" name="serie" :value="old('serie')" required/>
+                                </div>
+
+
+                                <div>
+                                    <x-label for="repeticao" :value="__('Quantidade de repetição')" />
+
+                                    <x-input id="repeticao" class="block mt-1 w-full" type="text" name="repeticao" :value="old('repeticao')" required/>
+                                </div>
+
+                                <div>
                                     <x-label for="descricao" :value="__('Descrição de treino')" />
 
                                     <x-input id="descricao" class="block mt-1 w-full" type="text" name="descricao" :value="old('descricao')" required/>
@@ -88,9 +125,9 @@ $fichas=App\Models\Ficha_treino::all();
 
 
                                 <div class="mt-4">
-                <x-label for="user_id" :value="__('Informe o id do aluno')" />
+                <x-label for="user_name" :value="__('Informe o nome do aluno')" />
 
-                <x-input id="user_id" class="block mt-1 w-full" type="number" name="user_id" :value="old('user_id')" required />
+                <x-input id="user_name" class="block mt-1 w-full" type="text" name="user_name" :value="old('user_name')" required />
             </div>
 
 
