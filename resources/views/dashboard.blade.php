8c374d2d30781cb0a25a998c7b37c6684bd252ef
Patrick-Santana <zonaabissal2016@gmail.com>
2021-03-02 14:07:00 -0300

    GH-22 Adicionando Layout no Dashbord de atendente

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,21 @@
-<style type="text/css">
-    
-</style>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Coliseu') }}
         </h2>
+        <center>
+            <a href="atendente/cadastroaluno"  class="bg-green-200 hover:bg--700 text-white font-bold py-2 px-4 rounded">Cadastrar Aluno</a>
+
+            <a href="atendente/cadastroprofessor" class="bg-green-200 hover:bg--700 text-white font-bold py-2 px-4 rounded">Cadastrar Professor</a>
+            
+            
+            <a href="atendente/cadastroequipamento" class="bg-green-200 hover:bg--700 text-white font-bold py-2 px-4 rounded" >Cadastrar Equipamento</a>    
+        </center>
     </x-slot>
 
-<a href="atendente/cadastroaluno">Cadastrar ou Alterar o Cadastro de Algum Aluno</a>
-<a href="atendente/cadastroprofessor">Cadastrar ou Alterar o Cadastro de Algum Professor</a>
-<a href="atendente/cadastroequipamento">Cadastrar ou Alterar o Cadastro de Algum Equipamento</a>
-
+    
 </x-app-layout>
+
+
 
