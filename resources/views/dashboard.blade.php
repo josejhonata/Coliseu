27d88018a169a681f15aaf53831d205bc08f3bbe
Patrick-Santana <zonaabissal2016@gmail.com>
2021-03-04 02:41:15 -0300

    GH-22 Update Layout Atendente

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,6 @@
 
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Coliseu') }}
-        </h2>
         <center>
             <a href="atendente/cadastroaluno"  class="bg-green-200 hover:bg--700 text-white font-bold py-2 px-4 rounded">Cadastrar Aluno</a>
 
@@ -13,8 +10,6 @@
             <a href="atendente/cadastroequipamento" class="bg-green-200 hover:bg--700 text-white font-bold py-2 px-4 rounded" >Cadastrar Equipamento</a>    
         </center>
     </x-slot>
-
-    
 </x-app-layout>
 
 
