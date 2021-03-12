a2a8ed1ddd6a9ca79b0d8407691eb40d0d1fc92a
Patrick-Santana <zonaabissal2016@gmail.com>
2021-03-11 22:20:37 -0300

    GH-22 Padronização de Estilo do Site

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,17 +22,18 @@
             <div>
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" placeholder="Ex: user@gmail.com" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Senha')" />
+                <x-label for="password"  :value="__('Senha')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="current-password" />
+                                required autocomplete="current-password" 
+                                placeholder="Ex: 12345678"/>
             </div>
 
 
@@ -41,6 +42,7 @@
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                
                 </label>
             </div>
 
@@ -51,7 +53,10 @@
                     </a>
                 @endif
 
-                <x-button class="ml-3" style="background: #d85c23">
+                
+                
+            <div class= "flex items-center justify-end mt-1 ml-4">
+                <x-button class="ml-3" class="bg-green-200 hover:bg--700 text-white font-bold py-2 px-4 rounded">
                     {{ __('Login') }}
                 </x-button>
             </div>
