60852e1de50116dbb4578781f557ebf9a024e9c9
josetrevisan04@gmail.com <josetrevisan04@gmail.com>
2021-03-12 11:57:33 -0300

    Adicionando o botão, mais visualizar

--- resources/views/cliente.blade.php
+++ resources/views/cliente.blade.php
@@ -21,9 +21,11 @@ border-right: 1px solid #ddd;
 
 
 @php
-$fichas=App\Models\Ficha_treino::all();
+$fichas=App\Models\Ficha_treino::where('user_name',Auth::user()->name)->get();
 $cliente=Auth::user()->id;
+$exercicios=App\Models\exercicio::all();
 @endphp
+
 
 	<x-slot name="header">
             <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -77,7 +79,8 @@ $cliente=Auth::user()->id;
         <tbody>
 
             @foreach ($fichas as $ficha)
-            <tr>
+
+                        <tr>
                 <td>{{$ficha->titulo}}</td>
                 <td>{{$ficha->data_inicio}}</td>
                 <td>{{$ficha->data_final}}</td>
@@ -85,13 +88,65 @@ $cliente=Auth::user()->id;
                 <td>{{$ficha->tipo_de_treino}}</td>
                 <td>{{$ficha->situacao}}</td>
                 <td>
-                    <a href="{{ route('visu-user', $ficha->id)}}">
+                    <div x-data="{add_modal:false}">
+                        <div @click= "add_modal = true">
                     <x-button class="ml-4">Visualizar</x-button>
-                    </a>
+                    </div>
 
-                    <a href="">
-                        <x-button class="ml-4">Imprimir</x-button>
-                    </a>
+                    
+<div class="fixed z-10 inset-0 overflow-y-auto" x-show="add_modal">
+  <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+
+    <div class="fixed inset-0 transition-opacity" aria-hidden="true" @click="add_modal = false">
+      <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+    </div>
+
+    <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+  
+    -->
+    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="sm:flex sm:items-start">
+    <div hidden>   
+@foreach ($fichas as $fichaa)
+{{$foi= $fichaa->id}}
+@endforeach
+</div>
+<center>
+<table class="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-800 text-gray-200">
+      <tr style="background: #131313; color :white;">
+    <th>qdq</th>
+    <th>dqwq</th>
+    <th>dqwdqw</th>
+    <th>dqqwd</th>
+    <th>dqwdq</th>
+    <th>dqwdq</th>
+    </tr>
+
+     @foreach ($exercicios as $exercicio)
+     @if ($foi == $exercicio->id_ficha)
+
+        <tbody>
+     <tr>
+     <td>{{$exercicio->equipamento}}</td>
+     <td>{{$exercicio->descricao}}</td>
+     <td>{{$exercicio->serie}}</td>
+     <td>{{$exercicio->repeticao}}</td>
+    <td>{{$exercicio->descanso}}</td>
+    <td>{{$exercicio->observacao}}</td>
+    </tr>
+     @endif
+     @endforeach 
+     </tbody> 
+     </table>  
+     </center>    
+    </div>
+  </div>
+</div>
+
+
+                    </div>
+                    
 
                 </td>
             </tr>
@@ -103,3 +158,5 @@ $cliente=Auth::user()->id;
 </center>
 
  </x-app-layout>
+
+ 
